ae2fce6cc3608dac76c51c78d72d0a7e26f4bdf5
Zeta-qixi <57753690+Zeta-qixi@users.noreply.github.com>
2021-03-18 08:50:51 +0800

    update
    把功能分开到不同文件

--- plugins/chat.py
+++ plugins/chat.py
@@ -26,15 +26,6 @@ data_ft = []
 data = {}
 ptalk = {}
 
-with open(path_tome) as f:
-    for line in f.readlines():
-        data_tome.append(line.strip())
-with open(path_nm) as f:
-    for line in f.readlines():
-        data_nm.append(line.strip())
-with open(path_ft) as f:
-    for line in f.readlines():
-        data_ft.append(line.strip())
 with open(path) as f:
     for line in f.readlines():
         k = line.split()
@@ -54,104 +45,29 @@ async def chat(context):
     user_id = context['user_id']
     
     global ptalk
+      #第一次对话初始化变量
     ptalk.setdefault(group_id,0.3)
-
     trigger.setdefault(group_id,' ')
 
-    if '老婆' in message and me in message :
-        if user_id == master[0]:
-            msg_list=['(〃ω〃)', '(///▽///)']
-            await bot.send_group_msg(group_id=group_id,message=random.choice(msg_list))
-        elif random.random() > 0.5:
-            msg_list=['?', '？？', '[CQ:face,id=32]']
-            await bot.send_group_msg(group_id=group_id,message=random.choice(msg_list))
-
-
-    for i in ['睡觉', '歇了','眠了']:
-        if i in message :
-            for j in ['不', '别','你', '他']:
-                if j not in message:
-                    if user_id in master:
-                        await bot.send_group_msg(group_id=group_id,message=f'[CQ:at,qq={user_id}] 晚安~ mua~')
-                        return 
-                    else:
-                        await bot.send_group_msg(group_id=group_id,message=' 晚安啦~')
-                        return 
-
-#1. 非master 发送 含触发词 信息
-#2. 对字符<2 用精准匹配
-#3. 是否已触发
-#4. 随意 & 主动对线
-
-
+'''
+4个if判定：
+1. 判断关键词 
+2. 对字符<2 用精准匹配
+3. 是否已触发
+4. 回复概率
+'''
     for i in data:
-        if i in message and user_id != master[0] :
+        if i in message :
             if len(i) > 2 or i == message:
                 if trigger[group_id] != i :
-                    if random.random() < ptalk[group_id] or user_id == 1109144843:
+                    if random.random() < ptalk[group_id]:
                         await bot.send_group_msg(group_id=group_id,message=random.choice(data[i]))
                         trigger[group_id] = i
                         return 0
 
 
-    if me in message and user_id not in master:
-        for i in data_nm :
-            if i in message:
-
-                if random.random() < 0.95:
-                    await bot.send_group_msg(group_id=group_id,message=random.choice(data_ft))
-                    return 0
-                else :
-                    await bot.send_group_msg(group_id=group_id,message='[CQ:face,id=146]')
-                    await bot.send_group_msg(group_id=group_id,message=f'[CQ:at,qq={user_id}] 今天的人品值是：0')
-                    return 0
-        tome = ['( ^ω^ )', '?', '¿', '......','(･_･;', '呵呵', '[CQ:face,id=13][CQ:face,id=13][CQ:face,id=13]','sb?']
-        await bot.send_group_msg(group_id=group_id,message=random.choice(data_tome))
-
-
-
-    
 
 ####--- 下面是 Command-- ####
-@on_command('set', aliases=('设置',),only_to_me=True)
-async def setword(session: CommandSession):
-    global lock
-    while lock >= 1:
-        await time.sleep(1)
-    lock = 1
-    comman = session.ctx['raw_message'].split()
-    try:
-        global data
-        #comman[0] 是 'set'
-        keys = comman[1]
-        #录入库
-        if keys not in data:
-            data.setdefault(keys,[])
-            data [keys] = comman[2:]
-        else:  
-            for i in comman[2:]:
-                if i not in data[keys]: 
-                    data[keys].append(i)
-        text = ''
-        for i in comman[2:]:    
-            text = text + i + ' '   
-        await session.send(message= f'已保存{keys}:{text}')
-    except:
-        await session.send(message= f'失败了，可以找master在后台导入')
-    lock = 0
-
-@on_command('save', aliases=('保存',),only_to_me=True)
-async def saveword(session: CommandSession):
-        with open(path, 'w+') as f :
-            for i in data:
-                f.write(f'{i} ')
-                words = ''
-                for ii in data[i]:
-                    words = words + ii +' '
-
-                f.write(f'{words}\n')
-        await session.send(message= 'ok~')
-
 
 @on_command('setP', aliases=('setp',),only_to_me=True)
 async def setP(session: CommandSession):
@@ -163,27 +79,9 @@ async def setP(session: CommandSession):
         if not msg:
             msg = session.get('message', prompt='设置概率~ \n--这是范围[0,1]')
         ptalk[group_id] = float(msg)
-        await session.send(message= f'你群现在的回复率为：{ptalk[group_id]}')
+        await session.send(message= f'现在的回复率为：{ptalk[group_id]}')
     else :
         await session.send(message= '不是master， 爬')
 
-
-@on_command('addmaster', only_to_me=True)
-async def addM(session: CommandSession):
-
-    user_id=session.ctx['user_id']
-    global master
-    if user_id == master[0]:
-        ##todo
-        msg=session.current_arg.strip()
-        if not msg:
-            msg = session.get('message', prompt='誰ですか~')
-        
-        p=r'\d+'
-        qq=re.findall(p,msg)
-        master.append(int(qq[0]))
-        await session.send(message= f'添加新master：{msg}')
-    else :
-        await session.send(message= '?')
 
 
