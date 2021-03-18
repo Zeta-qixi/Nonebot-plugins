b79f403890bf4f7e72e34fb53e8d4bbd06477ab9
Zeta-qixi <57753690+Zeta-qixi@users.noreply.github.com>
2021-03-18 13:58:38 +0800

    Update chat.py
    改用json保存语料

--- plugins/chat.py
+++ plugins/chat.py
@@ -1,42 +1,28 @@
 import nonebot
 from nonebot import on_command, CommandSession, Message
-from nonebot import on_natural_language, NLPSession, IntentCommand
 import os
 import sys
+import json
 import random
 import time
 import re
 from aiocqhttp.exceptions import ActionFailed
 
-#变量
-path_tome ='data/chat/tome.txt'
-path_nm ='data/chat/nm.txt'
-path_ft ='data/chat/ft.txt'
-path = 'data/chat/data.txt'
 bot = nonebot.get_bot()
 master = bot.config.MASTER
-me = '894296015'
-      
+   
 # -global- #
 trigger = {}
 lock = 0
-data_tome = []
-data_nm = []
-data_ft = []
-data = {}
-ptalk = {}
+data = {} 
+ptalk = {} #回复率
 
+
+gpath =os.path.dirname(__file__)
+#语料库地址
+path = gpath +'/data.json'
 with open(path) as f:
-    for line in f.readlines():
-        k = line.split()
-        data[k[0]] = k[1:]
-        
-'''
-1.判定内容
-    判定是否@ （属于msg
-        判定是否master
-            to do 
-'''
+    data = json.load(f)
 
 @bot.on_message('group')
 async def chat(context):
@@ -45,43 +31,66 @@ async def chat(context):
     user_id = context['user_id']
     
     global ptalk
-      #第一次对话初始化变量
-    ptalk.setdefault(group_id,0.3)
+    ptalk.setdefault(group_id,0.1)
     trigger.setdefault(group_id,' ')
 
-'''
-4个if判定：
-1. 判断关键词 
-2. 对字符<2 用精准匹配
-3. 是否已触发
-4. 回复概率
-'''
     for i in data:
-        if i in message :
-            if len(i) > 2 or i == message:
+        if i in message and user_id != master[0] :
+            #长度大于3模糊匹配
+            if len(i) > 3 or i == message:
                 if trigger[group_id] != i :
-                    if random.random() < ptalk[group_id]:
+                    if random.random() < ptalk[group_id] :
+                        time.sleep(random.randrange(5))
                         await bot.send_group_msg(group_id=group_id,message=random.choice(data[i]))
                         trigger[group_id] = i
                         return 0
 
+'''
+下面是 Command
 
+'''
 
-####--- 下面是 Command-- ####
+#添加语录
+@on_command('set', aliases=('设置',),only_to_me=True)
+async def setword(session: CommandSession):
 
+    comman = session.ctx['raw_message'].split()
+    try:
+        global data
+        #comman[0] 是 'set'
+        keys = comman[1]
+        #录入库
+        if keys not in data:
+            data.setdefault(keys,[])
+            data [keys] = comman[2:]
+        else:  
+            for i in comman[2:]:
+                if i not in data[keys]: 
+                    data[keys].append(i)
+        
+        with open(path, 'w+') as f :
+            tojson = json.dumps(data,sort_keys=True, ensure_ascii=False, indent=4,separators=(',',': '))
+            f.write(tojson)
+
+        text = ''
+        for i in comman[2:]:    
+            text = text + i + ' '   
+        await session.send(message= f'已保存{keys}:{text}')
+    except:
+        await session.send(message= f'失败了QAQ')
+
+#设置回复率
 @on_command('setP', aliases=('setp',),only_to_me=True)
 async def setP(session: CommandSession):
     group_id = session.ctx['group_id']
     user_id=session.ctx['user_id']
-    if user_id in master:
+    if user_id == master[0]:
         msg=session.current_arg.strip()
         global ptalk
         if not msg:
-            msg = session.get('message', prompt='设置概率~ \n--这是范围[0,1]')
+            msg = session.get('message', prompt='设置概率~ \n--范围[0,1]')
         ptalk[group_id] = float(msg)
-        await session.send(message= f'现在的回复率为：{ptalk[group_id]}')
+        await session.send(message= f'你群现在的回复率为：{ptalk[group_id]}')
     else :
-        await session.send(message= '不是master， 爬')
-
-
+        await session.send(message= '?')
 
